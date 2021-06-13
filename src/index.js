2b5048668609ebdf8b2c62f44f5ab596d9997c19
JahedHossenBangladesh <jahedhossen4358.com>
2021-06-14 01:33:53 +0600

    This is Jsx and the react dom rendering process

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(() => {
+    const element = (
+        <h1 className="heading">
+            HI <span>{new Date().toLocaleTimeString()}</span>
+        </h1>
+    );
+    ReactDom.render(element, document.getElementById('root'));
+}, 1000);
