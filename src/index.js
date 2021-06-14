0a677ac283248c9b54e18ce85aba6ae5b3c05a6c
JahedHossenBangladesh <jahedhossen4358.com>
2021-06-15 02:48:55 +0600

    This is end of the components and props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function Clock() {
-    return (
-        <h1 className="heading">
-            HI <span>{new Date().toLocaleTimeString()}</span>
-        </h1>
-    );
+// eslint-disable-next-line react/prefer-stateless-function
+class Clock extends React.Component {
+    render() {
+        const { locale, children } = this.props;
+        return (
+            <div>
+                <h1>Hello, worlddddddddddd!</h1>
+
+                <h2>It is {new Date().toLocaleTimeString(locale)}.</h2>
+                <h3>{children}</h3>
+            </div>
+        );
+    }
 }
 
-ReactDom.render(Clock(), document.getElementById('root'));
+ReactDom.render(<Clock locale="bn-BD">this is me </Clock>, document.getElementById('root'));
